07c7e9241e85368d227337eb21310285973c6ba0
FrolkinMax <frolkinmax@mail.ru>
2024-11-30 01:40:05 +0700

    Add files via upload
    "Создание функций на лету"
    Цель: освоить на практике замыкание, объекты-функторы и lambda-функции.
    
    Задача "Функциональное разнообразие":
    Lambda-функция:
    Даны 2 строки:
    first = 'Мама мыла раму'
    second = 'Рамена мало было'
    Необходимо составить lambda-функцию для следующего выражения - list(map(?, first, second)).
    Здесь ? - место написания lambda-функции.
    
    Результатом должен быть список совпадения букв в той же позиции:
    [False, True, True, False, False, False, False, False, True, False, False, False, False, False]
    Где True - совпало, False - не совпало.
    
    Замыкание:
    Напишите функцию get_advanced_writer(file_name), принимающую название файла для записи.
    Внутри этой функции, напишите ещё одну - write_everything(*data_set), где *data_set - параметр принимающий неограниченное количество данных любого типа.
    Логика write_everything заключается в добавлении в файл file_name всех данных из data_set в том же виде.
    Функция get_advanced_writer возвращает функцию write_everything.
    
    Данный код:
    write = get_advanced_writer('example.txt')
    write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
    Запишет данные в файл в таком виде:
    
    
    Метод __call__:
    Создайте класс MysticBall, объекты которого обладают атрибутом words хранящий коллекцию строк.
    В этом классе также определите метод __call__ который будет случайным образом выбирать слово из words и возвращать его. Для случайного выбора с одинаковой вероятностью для каждого данного в коллекции можете использовать функцию choice из модуля random.
    
    Ваш код (количество слов для случайного выбора может быть другое):
    from random import choice
    # Ваш класс здесь
    first_ball = MysticBall('Да', 'Нет', 'Наверное')
    print(first_ball())
    print(first_ball())
    print(first_ball())
    Примерный результат (может отличаться из-за случайности выбора):
    Да
    Да
    Наверное
    
    Примечания:
    Все задания пишутся в одном модуле.
    Передаваемые данные в функции и объекты можете использовать свои, главное, чтобы ваш код полноценно демонстрировал логику написанного.

---
+++ module_9_4.py
@@ -0,0 +1,54 @@
+print()
+print('\033[36;4mАвтор: Фролкин Максим\033[0m','\n')
+print('\033[36;4mЗадача "Создание функций на лету"\033[0m','\n')
+
+# Цель: освоить на практике замыкание, объекты-функторы и lambda-функции.
+# Задача "Функциональное разнообразие":
+
+# 1) Lambda-функция:
+# Даны 2 строки:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+# Решение:
+func_v1 = lambda list_1, list_2: list_1 == list_2
+result = list(map(func_v1, first, second))
+
+# Пример выполнения кода:
+print(result)
+# Результат:
+# [False, True, True, False, False, False, False, False, True, False, False, False, False, False]
+
+# 2) Замыкание:
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            # Записываем каждый элемент из data_set в файл
+            for item in data_set:
+                file.write(str(item) + '\n')
+        # Возвращаем внутреннюю функцию write_everything
+    return write_everything
+
+# Пример выполнения кода:
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+# Результат:
+# Это строчка
+# ['А', 'это', 'уже', 'число', 5, 'в', 'списке']
+
+# 3) Метод __call__:
+from random import choice
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+# Пример выполнения кода:
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
