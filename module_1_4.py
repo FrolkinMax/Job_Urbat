13fa7f63226e0dbee346f3a70b7ad7a40d4b534e
FrolkinMax <frolkinmax@mail.ru>
2025-01-06 01:08:21 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,18 @@
+
+print()
+print('\033[36;4mАвтор: Фролкин Максим\033[0m','\n')
+print('\033[36;4mПрактическое задание по теме "Переменные"\033[0m','\n')
+
+Haus_job = 12
+time_job = 1.5
+Name_course = 'Python'
+time_one_job = (time_job/Haus_job)
+print(f'Курс: {Name_course}, всего задач: {Haus_job}, затрачено часов: {time_job}, среднее время выполнения {time_one_job} час')
+print()
+
+first_name = 'Vasya'
+apple_count = 10
+second_name = 'Petya'
+orange_count = 15
+print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+
