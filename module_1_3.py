0e8891b1c1ae809c57489a6e396addb174ae9518
FrolkinMax <frolkinmax@mail.ru>
2025-01-06 00:40:02 +0700

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"
    
    Цель: Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.
    
    В проекте, где вы решаете домашние задания, создайте модуль 'module_1_3.py' и напишите весь код в нём.
    
    2. Создайте переменные разных типов данных:
      - Создайте переменную name и присвойте ей значение вашего имени (строка).
      - Выведите значение переменной name на экран.
      - Создайте переменную age и присвойте ей значение вашего возраста (целое число).
      - Выведите значение переменной age на экран.
      - Перезапишите в age текущее значение переменной age + новое.
    Как неверно (просто перезапись на новое число):
    a = 15
    a = 17
      - Выведите измененное значение переменной age на экран.
      - Создайте переменную is_student и присвойте ей значение True (логическое значение).
      - Выведите значение переменной is_student на экран.
    
    Примечания:
    - Для вывода значений на экран используйте функцию print().
    - Обратите внимание на использование разных типов данных и возможности их изменения.
    
    Пример результата выполнения программы:
    Name: John
    Age: 25
    New Age: 26
    Is Student: True

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+
+print()
+print('\033[36;4mАвтор: Фролкин Максим\033[0m','\n')
+print('\033[36;4mПрактическая работа по уроку "Динамическая типизация"\033[0m','\n')
+
+name = 'Max'
+print(name)
+age = 37
+print(age)
+New_age = 38
+print(New_age)
+is_student = True
+print(is_student)
